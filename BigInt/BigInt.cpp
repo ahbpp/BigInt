f8a10fb765a4d87b41a16d9e7025f8be33043cc8
ahbpp <alexeykarpov@MacBook-Air-Alexey.local>
2017-03-14 19:07:03 +0300

    begin

---
+++ BigInt/BigInt.cpp
@@ -0,0 +1,9 @@
+//
+//  BigInt.cpp
+//  BigInt
+//
+//  Created by Alexey Karpov on 14.03.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include "BigInt.hpp"
---
+++ BigInt/BigInt.hpp
@@ -0,0 +1,14 @@
+//
+//  BigInt.hpp
+//  BigInt
+//
+//  Created by Alexey Karpov on 14.03.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#ifndef BigInt_hpp
+#define BigInt_hpp
+
+#include <stdio.h>
+
+#endif /* BigInt_hpp */
