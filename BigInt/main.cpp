5482bd986c5341d752bcc439c6214a86155bc693
ahbpp <alexeykarpov@MacBook-Air-Alexey.local>
2017-03-14 19:06:07 +0300

    Initial Commit

---
+++ BigInt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BigInt
+//
+//  Created by Alexey Karpov on 14.03.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
